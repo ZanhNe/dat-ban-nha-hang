1796c691101d28cae1843adb85e11d3db0e3f2ba
NguyenAnhCam10 <nguyenanhcam10@gmail.com>
2026-03-15 18:19:00 +0700

    frontend base

---
+++ frontend/dat-ban-nha-hang/src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import Header from '../../components/Header/Header'
+import RestaurantHero from "../../components/RestaurantHero/RestaurantHero"
+import MenuSection from "../../components/MenuSection/MenuSection"
+import "./Home.css"
+function Home() {
+    return (
+        <>
+            <Header />
+            <RestaurantHero />
+            <div className="content-layout">
+                <div className="menu-area">
+                    <MenuSection />
+                </div>
+
+                <div className="map-area">
+                    MAP
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
